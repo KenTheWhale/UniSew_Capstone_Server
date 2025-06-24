34e1f81e3859a840076c7579f269acfa1cf6d694
Trieu <nguyenngoctram762@gmail.com>
2025-06-25 00:46:32 +0700

    feat: add profile service

---
+++ server/src/main/java/com/unisew/server/requests/UpdatePackageRequest.java
@@ -0,0 +1,26 @@
+package com.unisew.server.requests;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdatePackageRequest {
+    Integer id;
+    String name;
+    String headerContent;
+    int deliveryDuration;
+    int revisionTime;
+    long fee;
+    Integer designerId;
+    List<Integer> serviceIds;
+}
