3e6053de5f5ef4b10ad55821d9ccff0ff3dda741
Trieu <nguyenngoctram762@gmail.com>
2025-08-06 02:56:25 +0700

    feat: Preauthorize at controller

--- src/main/java/com/unisew/server/controllers/OrderController.java
+++ src/main/java/com/unisew/server/controllers/OrderController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,26 +20,31 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("")
+    @PreAuthorize("hasAuthority('GARMENT')")
     public ResponseEntity<ResponseObject> createOrder(@RequestBody CreateOrderRequest request) {
         return orderService.createOrder(request);
     }
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('GARMENT')")
     public ResponseEntity<ResponseObject> viewOrder() {
         return orderService.viewOrder();
     }
 
     @PostMapping("/quotation")
+    @PreAuthorize("hasAuthority('GARMENT')")
     public ResponseEntity<ResponseObject> createQuotation(HttpServletRequest httpServletRequest, @RequestBody QuotationRequest request) {
         return orderService.createQuotation(httpServletRequest, request);
     }
 
     @GetMapping("/quotation/{orderId}")
+    @PreAuthorize("hasAuthority('GARMENT')")
     public ResponseEntity<ResponseObject> viewQuotation(@PathVariable(name = "orderId") int orderId) {
         return orderService.viewQuotation(orderId);
     }
 
     @GetMapping("/quotation/approval/{quotationId}")
+    @PreAuthorize("hasAuthority('GARMENT')")
     public ResponseEntity<ResponseObject> approveQuotation(@PathVariable(name = "quotationId") int quotationId) {
         return orderService.approveQuotation(quotationId);
     }
