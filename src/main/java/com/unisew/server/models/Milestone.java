52fcea23eb00092c79912e42841aa22c496c99e2
KenTheWhale <kienquoc11026789@gmail.com>
2025-08-12 14:45:21 +0700

    merge: merge code

---
+++ src/main/java/com/unisew/server/models/Milestone.java
@@ -0,0 +1,42 @@
+package com.unisew.server.models;
+
+import com.unisew.server.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "`milestone`")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Milestone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "`phase_id`")
+    SewingPhase phase;
+
+    @ManyToOne
+    @JoinColumn(name = "`order_id`")
+    Order order;
+
+    @Column(name = "`start_date`")
+    LocalDate startDate;
+
+    @Column(name = "`end_date`")
+    LocalDate endDate;
+
+    @Column(name = "`image_url`")
+    String imgUrl;
+
+    @Enumerated(EnumType.STRING)
+    Status status;
+}
